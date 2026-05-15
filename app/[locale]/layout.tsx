a35ef41c85fa44ae0898ff05e2d3f356e44e07a1
Herodesjosue <herodeslugo@gmail.com>
2026-05-14 22:35:45 -0400

    refactor: update routing import path to use @ alias in layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { getMessages, getTranslations } from "next-intl/server";
 import { notFound } from "next/navigation";
-import { routing } from "../../i18n/routing";
+import { routing } from "@/i18n/routing";
 import "../globals.css";
 
 const geistSans = Geist({
